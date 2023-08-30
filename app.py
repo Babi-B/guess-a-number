9c8908e114bb52c7ea7eccf9b23678d6cce5f097
Babi-B <babibyoolah>
2023-08-30 12:30:29 +0100

    guessing game complete!

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
-print("Hello, world!")
+import intro
+import numGenerator
+from time import sleep
+
+intro
+numGenerator
+random_number = numGenerator.random_number
+
+print("")
+print("DONE!")
+print("")
+sleep(.2)
+user_input = int(input("Now Guess The Number ;D : "))
+
+
+while user_input != random_number:
+    if user_input > random_number:
+        sleep(.2)
+        print("")
+        print("Aish! Too high")
+        user_input = int(input("Guess again!: "))
+    if user_input < random_number:
+        sleep(.2)
+        print("")
+        print("Tsk! Too low")
+        user_input = int(input("Guess again!: "))
+
+print("")
+print("SUPER! You Win")
+
+
+
+
